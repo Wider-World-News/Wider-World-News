d4132bd18f9174c31ec474efaf18a375591a9816
Brian Kang <brkang1@gmail.com>
2021-09-23 17:20:43 -0400

    added integration tests for routes

---
+++ __tests__/supertest.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const app = require('../server/server');
+const server = 'http://localhost:3000';
+
+
+describe('routing tests', () => 
+{
+  describe('GET', () => 
+  {
+    describe('/', () => 
+    {
+      it('returns an html file', async () => {
+        const response = await request(server).get('/');
+        expect(response.headers['content-type']).toBe('text/html; charset=UTF-8');
+      });
+      
+      it('returns with a status code of 200', () => {
+        request(server).get('/')
+        .expect(200);
+      });
+    });
+  });
+});
