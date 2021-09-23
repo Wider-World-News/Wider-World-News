21cfa9d6701d4173a7cc832f57c87b0c9feb295f
Brian Kang <brkang1@gmail.com>
2021-09-23 15:27:48 -0400

    try disconnecting first before connecting test

--- tests/user.model.test.js
+++ tests/user.model.test.js
@@ -3,11 +3,10 @@ const {Users} = require('../server/models/mapModels');
 
 const dbURI = 'mongodb://127.0.0.1/testDB'
 
-require("dotenv").config();
-jest.setTimeout(6000);
 //before any test, delete the test db
 beforeAll(async () => {
   try {
+    await mongoose.disconnect();
     await mongoose.connect(dbURI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
